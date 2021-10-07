8789b11ebf2969fcb4964f2c42304cc7757190b9
Thalison Campos <thalison1998@gmail.com>
2021-10-07 01:04:51 -0300

    Refatoração dos modulos para factory function

---
+++ assets/js/modules/dropDown.js
@@ -0,0 +1,27 @@
+import clickOut from "./clickOut";
+export default function dropdownMenu(listDrop) {
+  const dropMenu = document.querySelectorAll(listDrop);
+  let events = ["click", "touchstart"];
+
+  const activeDropDown = (e) => {
+    const element = e.currentTarget;
+    element.classList.add("on");
+
+    clickOut(element, events, () => {
+      element.classList.remove("on");
+    });
+  };
+
+  const init = () => {
+    if (!!dropMenu) {
+      dropMenu.forEach((item) => {
+        events.forEach((event) => {
+          item.addEventListener(event, activeDropDown);
+        });
+      });
+    }
+  };
+  return Object.freeze({
+    init,
+  })
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
-import menuDropDown from "./modules/menuDropDown.js"
-menuDropDown()
+import dropdownMenu from './modules/dropDown';
+import menuMobile from './modules/menu-mobile';
+
+const menuMob = menuMobile('[data-btnmobile]','[data-navigationmobile]');
+menuMob.init()
+const dropMenu = dropdownMenu("[data-dropmenu]");
+dropMenu.init()
+
 
